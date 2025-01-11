a90921ab1ed1546d863ca0fd63b40cb965ab2231
Cyrus987 <tfish3029@gmail.com>
2025-01-11 01:09:49 +0000

    finished

--- main.java
+++ main.java
@@ -1,10 +1,11 @@
 public class main{
-  public static void main(String[] args){
-
-    for(int i = 0; i<1000; ){
-
-    }
-
-  }
+  public static void main(String args[]){
+    int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
 }
-
+}
